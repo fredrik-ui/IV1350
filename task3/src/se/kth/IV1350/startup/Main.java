fd2a776b20201b18c2f3aab0df03f8b91a4ab00d
Fredrik Kis <fredrik.kis03@gmail.com>
2024-04-21 22:14:23 +0200

    added bin folder

--- task3/src/se/kth/IV1350/startup/Main.java
+++ task3/src/se/kth/IV1350/startup/Main.java
@@ -9,10 +9,9 @@ import se.kth.IV1350.view.View;
 public class Main {
     
     // To compile everything:
-    // javac -classpath . se/kth/IV1350/controller/*.java se/kth/IV1350/integration/*.java se/kth/IV1350/model/*.java se/kth/IV1350/startup/*.java se/kth/IV1350/view/*.java
+    // javac -classpath . -d bin src/se/kth/IV1350/controller/*.java src/se/kth/IV1350/integration/*.java src/se/kth/IV1350/model/*.java src/se/kth/IV1350/startup/*.java src/se/kth/IV1350/view/*.java
     // To run:
-    // java -classpath src se.kth.IV1350.startup.Main
-
+    // java -classpath bin se.kth.IV1350.startup.Main
     public static void main(String[] args){
         ExternalDB externalSystems = new ExternalDB();
         ReceiptPrinter printer = new ReceiptPrinter();
