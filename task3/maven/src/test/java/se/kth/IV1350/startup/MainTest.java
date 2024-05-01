854d6737d03d88193f1df3995e166acdf06cf15b
Fredrik Kis <fredrik.kis03@gmail.com>
2024-05-01 15:51:05 +0200

    Small fixes

--- task3/maven/src/test/java/se/kth/IV1350/startup/MainTest.java
+++ task3/maven/src/test/java/se/kth/IV1350/startup/MainTest.java
@@ -1,20 +1,5 @@
 package se.kth.IV1350.startup;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-
 public class MainTest {
-    private 
-
-    @BeforeEach
-    void setUp(){
-
-    }
-
-    @AfterEach
-    void tearDown(){
-
-    }
-
 
 }
--- task3/maven/src/test/java/se/kth/IV1350/view/ViewTest.java
+++ task3/maven/src/test/java/se/kth/IV1350/view/ViewTest.java
@@ -1,17 +1,4 @@
 package se.kth.IV1350.view;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import se.kth.IV1350.integration.ExternalDB;
-import se.kth.IV1350.integration.ReceiptPrinter;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 public class ViewTest {
     
-    @BeforeEach
-    void setUp() {
-        //Controller contr = new Controller(new ExternalDB(), new ReceiptPrinter());
-    }
 }
